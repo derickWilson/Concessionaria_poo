13914f30536721591eea9749f288578855c3c136
derick wilson pereira do nascimento <108161829+derickWilson@users.noreply.github.com>
2023-11-26 13:53:43 +0000

    Update Frota.java

--- Frota.java
+++ Frota.java
@@ -1,6 +1,51 @@
-for(Veiculo i : frota){
+<<<<<<< main
+import java.util.ArrayList;
+public class Frota{
+    private ArrayList<Veiculo> frota = new ArrayList<Veiculo>();
+
+    public Frota(Veiculo veiculo)){
+        this.insertVeiculo(veiculo);
+    }
+
+    public int insertVeiculo(Veiculo veiculo){
+        try {//troca essa merda
+            if(!existePlaca(veiculo.getPlaca())){
+                frota.add(veiculo);
+                return 0;
+            }else{
+                System.out.println("o veiculo ja esta na frota");
+                return 1;
+            }
+        } catch (Exception e) {
+            System.err.println(e.toString());
+            return 2;
+        }
+    }
+//metodo para checar formato da placa
+    private boolean existePlaca(String placa){
+                
+    }
+    
+        //loop para percorrer a frota
+        for(Veiculo i : frota){
+
+            //se a placa ja estiver em um veiculo da frota retorna true
             if(i.getPlaca() == placa){
+                return true;
+            }else{
+                return false;
+            }
+        }
+
+        //caso a placa não esteja na frota retorna false, o objeto não foi cadastrado
+        return false;
+    }
+
+    public boolean removeVeiculo(String placa){
+        for(Veiculo i : frota){//terminar
+             if(i.getPlaca() == placa){
                 frota.remove(i);
                 return true;
             }
         }
+    }
